9e7fa5d1b8cb766e6dd7a35fdcd10acb7fbc2396
sbytex <32998028+sbytex@users.noreply.github.com>
2018-03-27 13:22:22 +0200

    Add files via upload
    The size of the environment now increases as new cells on its edges become alive.

--- environment.py
+++ environment.py
@@ -10,25 +10,29 @@ class Environment:
         for i in range(rows):
             r = []
             for n in range(columns):
-                r.append('□')
+                r.append(' ')
                 self.alive[(i, n)] = False
             self.matrix.append(r)
 
     # Sets certain cell alive
     def set_alive(self, row, column):
-        self.matrix[row][column] = '■'
+        self.matrix[row][column] = '□'
         self.alive[(row, column)] = True
+        if row == len(self.matrix) - 1:
+            self.add_row()
+        if column == len(self.matrix[0]) - 1:
+            self.add_column()
 
     # Sets certain cell dead
     def set_dead(self, row, column):
-        self.matrix[row][column] = '□'
+        self.matrix[row][column] = ' '
         self.alive[(row, column)] = False
 
     # Prints environment
     def print_env(self):
         os.system('cls')
         for i in range(len(self.matrix)):
-            for n in range(len(self.matrix[0])):
+            for n in range(len(self.matrix[i])):
                 print(self.matrix[i][n], end='')
             print()
 
@@ -48,6 +52,10 @@ class Environment:
             if s:
                 ppl += 1
         return ppl
+
+    # Returns size of matrix as string
+    def size(self):
+        return str(len(self.matrix)) + 'x' + str(len(self.matrix[0]))
 
     # Returns number of certain cell's alive neighbors
     def alive_neighbors(self, row, column):
@@ -69,3 +77,17 @@ class Environment:
         if column < len(self.matrix[0]) - 1 and row < len(self.matrix) - 1 and self.alive[(row + 1, column + 1)]:
             n_alive += 1
         return n_alive
+
+    # Expands matrix by new row of dead cells
+    def add_row(self):
+        row = []
+        for i in range(len(self.matrix[0])):
+            row.append(' ')
+            self.alive[(len(self.matrix), i)] = False
+        self.matrix.append(row)
+
+    # Expands matrix by new column of dead cells
+    def add_column(self):
+        for i in range(len(self.matrix)):
+            self.alive[(i, len(self.matrix[i]))] = False
+            self.matrix[i].append(' ')
--- game_of_life.py
+++ game_of_life.py
@@ -1,20 +1,19 @@
 from environment import Environment
 from time import sleep
 import random
+import copy
 
-# Environment of size 18x36
-rows = 18
-columns = 36
+# Initial environment of size rows*columns
+rows = 5
+columns = 10
 env = Environment(rows, columns)
 
 
 # Plus one generation
 def t_plus_1():
-    old_env = Environment(rows, columns)
-    old_env.matrix = list(env.matrix)
-    old_env.alive = dict(env.alive)
-    for i in range(rows):
-        for n in range(columns):
+    old_env = copy.copy(env)
+    for i in range(len(old_env.matrix)):
+        for n in range(len(old_env.matrix[0])):
             if old_env.alive_neighbors(i, n) < 2 or old_env.alive_neighbors(i, n) > 3:
                 env.set_dead(i, n)
             elif not old_env.alive[(i, n)] and old_env.alive_neighbors(i, n) == 3:
@@ -48,5 +47,6 @@ def start(delay=1):
         t += 1
         print('\nTime: ' + str(t))
         print('Population: ' + str(env.population()))
+        print('Size: ' + str(env.size()))
         sleep(delay)
     print('No living cell left.')
